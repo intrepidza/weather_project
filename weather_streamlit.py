4e2ac90e286bc69e70e28e58e6d9ece4fae76705
intrepidza <timthezombie@gmail.com>
2025-08-20 21:04:25 +0200

    Fixes... V2

--- weather_streamlit.py
+++ weather_streamlit.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-def generate_streamlit_data(file):
+
+def generate_streamlit_data():
     df = pd.read_csv('test.csv')
 
     st.title("Mobile Dash")
@@ -12,3 +13,10 @@ def generate_streamlit_data(file):
 
     fig2 = px.scatter(df, x="time", y="precipitation_probability")
     st.plotly_chart(fig2, use_container_width=True)
+
+    fig3 = px.scatter(df, x="time", y="relative_humidity_2m")
+    st.plotly_chart(fig3, use_container_width=True)
+
+
+if __name__ == '__main__':
+    generate_streamlit_data()
